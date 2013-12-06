94580edbaf64279ce3c219395cb4c582cb01c738
Luca Buonocore <luca.buonocore82@gmail.com>
2013-12-06 12:16:17 +0100

    Funzione Pins su Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131204113745) do
+ActiveRecord::Schema.define(version: 20131205111108) do
+
+  create_table "pins", force: true do |t|
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
